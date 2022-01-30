9dd5fc4de343c21117b5804e0ebb7fa154a3cc03
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-01-30 09:58:54 -0300

    Initial rest endpoint development with gin

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -22,7 +22,6 @@ func Hellos(names []string) (map[string]string, error) {
     messages := make(map[string]string)
     for _, name := range names {
         message, err := Hello(name)
-        if err != nil {
             return nil, err
         }
 
