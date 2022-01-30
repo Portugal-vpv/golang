f4034fc43f7df93484bce67baab84ef426161d8b
Victor Portugal <victorvasconcellos5@hotmail.com>
2022-01-30 08:42:49 -0300

    Finalize random message feature.

--- hello/hello.go
+++ hello/hello.go
@@ -2,22 +2,27 @@ package main
 
 import (
     "fmt"
-	"log"
+    "log"
 
     "example.com/greetings"
 )
 
 func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+    // Request a greeting message.
+    message, err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	if (err != nil) {
-		log.Fatal(err)
-	}
-
-    // Get a greeting message and print it.
-    // message := greetings.Hello("Slash")
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
